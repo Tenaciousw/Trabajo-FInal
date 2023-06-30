810ea3395511928ee2269d9ea672d31a574d9292
CPetruzzo <njw@live.com.ar>
2023-06-30 17:12:08 -0300

    Clase 2 terminada

--- proyecto-final/src/app/person-administration/person-administration.module.ts
+++ proyecto-final/src/app/person-administration/person-administration.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonDetailComponent } from './person-detail/person-detail.component';
 import { FormsModule } from '@angular/forms';
+import { PersonListComponent } from './person-list/person-list.component';
 
 
 
 @NgModule({
   declarations: [
-    PersonDetailComponent
+    PersonDetailComponent,
+    PersonListComponent
   ],
-  exports: [PersonDetailComponent],
+  exports: [PersonDetailComponent, PersonListComponent],
   imports: [
     CommonModule,
     FormsModule,
